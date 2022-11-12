1aeb607215103e316f5996461c3f124e161b7edd
ixavy <ict@ixavy.com>
2022-11-12 12:55:47 +0100

    Image pilots added to assets

---
+++ 01-MiPrimeraApp/01-MiPrimeraApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  01-MiPrimeraApp
+//
+//  Created by iXavy on 12/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 01-MiPrimeraApp/01-MiPrimeraApp/_1_MiPrimeraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_MiPrimeraAppApp.swift
+//  01-MiPrimeraApp
+//
+//  Created by iXavy on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct _1_MiPrimeraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
